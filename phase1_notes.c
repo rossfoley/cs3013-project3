a94f0210ec62f87bcd0a70014807334eb1b731c0
lgmclaughlin <lgmclaughlin@wpi.edu>
2013-09-23 18:55:19 -0700

    Updated notes

--- phase1_notes.c
+++ phase1_notes.c
@@ -1,19 +1,55 @@
+mutex mutex;
+int[PIRATE_COUNT] piratesInRoom;
+int[NINJA_COUNT] ninjasInRoom;
+int ninjaAccess;
+int pirateAccess;
+
 // Ninja Code
+int ninjaID = 0; // Some real value
 while(TRUE) {
 	// Code to enter the costume department
 	down(mutex);
-	if (ninjaAccess) {
+	if (ninjaAccess && !ninjasInRoom[ninjaID]) {
 		ninjas++;
-		pirateAccess = false;
+		ninjasInRoom[ninjaID] = TRUE;
 	}
 	up(mutex);
 
 	// Code to get dressed and leave
 	down(mutex);
-	ninjas--;
-	if (ninjas == 0) {
-		ninjaAccess = false;
-		pirateAccess = true;
+	if (ninjaAccess && ninjasInRoom[ninjaID]) {
+		ninjas--;
+		ninjasInRoom[ninjaID] = FALSE;
+
+		if (ninjas == 0) {
+			ninjaAccess = FALSE;
+			pirateAccess = TRUE;
+		}
+	}
+	up(mutex);
+}
+
+// Pirate Code
+int pirateID = 0; // Some real value
+while(TRUE) {
+	// Code to enter the costume department
+	down(mutex);
+	if (pirateAccess && !piratesInRoom[pirateID]) {
+		pirates++;
+		piratesInRoom[pirateID] = TRUE;
+	}
+	up(mutex);
+
+	// Code to get dressed and leave
+	down(mutex);
+	if (pirateAccess && piratesInRoom[pirateID]) {
+		pirates--;
+		piratesInRoom[pirateID] = FALSE;
+		
+		if (pirates == 0) {
+			ninjaAccess = TRUE;
+			pirateAccess = FALSE;
+		}
 	}
 	up(mutex);
 }
